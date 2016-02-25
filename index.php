2d7b1a648989a3a8a43b7394e3a391a6d7c23729
Kush Patel <patel.kush.d@gmail.com>
2016-02-25 07:16:21 -0500

    setting up the different tabbing and page load based on the URL you hit

--- index.php
+++ index.php
@@ -13,42 +13,56 @@
 </head>
 
 <body>
+  <?php
+    $whichPage = "home";
+    if($_SERVER['SERVER_NAME'] == "http://p1.kushsdwa15.xyz/") {
+      $whichPage = "project1";
+    } elseif ($_SERVER['SERVER_NAME'] == "http://p2.kushsdwa15.xyz/") {
+      $whichPage = "project2";
+    } elseif ($_SERVER['SERVER_NAME'] == "http://p3.kushsdwa15.xyz/") {
+      $whichPage = "project3";
+    } elseif ($_SERVER['SERVER_NAME'] == "http://p4.kushsdwa15.xyz/") {
+      $whichPage = "project4";
+    }
+  ?>
   <div class="container">
 
     <!-- Nav tabs -->
     <ul class="nav nav-tabs" role="tablist">
-      <li class="active">
+      <li class="<?php echo $whichPage=='home' ? ' active' : ''?>">
           <a href="#home" role="tab" data-toggle="tab">
               <icon class="glyphicon glyphicon-home"></icon>
           </a>
       </li>
-      <li>
+      <li class="<?php echo $whichPage=='project1' ? ' active' : ''?>">
         <a href="#profile" role="tab" data-toggle="tab">
           <i class="fa fa-user"></i> Profile
+          </a>
+      </li>
+      <li class="<?php echo $whichPage=='project2' ? ' active' : ''?>">
+        <a href="#project2" role="tab" data-toggle="tab">
+          <i class="fa fa-user"></i> Project 2
           </a>
       </li>
     </ul>
 
     <!-- Tab panes -->
     <div class="tab-content">
-      <div class="tab-pane fade active in" id="home">
-        <h1>Project 1</h1>
+      <div class="tab-pane fade <?php echo $whichPage=='home' ? ' active in' : ''?>" id="home">
         <p>The server you have reached is:
-          <?php
-          echo $_SERVER['SERVER_NAME'];
-          ?>
+          <?php echo $_SERVER['SERVER_NAME']; ?>
         </p>
         <h4><b><u>Appendix:</u></b></h4>
         <div class="col-md-4">
           <div class="list-group">
-            <a class="list-group-item active" href="http://p1.kushsdwa15.xyz/">Project 1</a>
-            <a class="list-group-item disabledLink" href="http://p2.kushsdwa15.xyz/">Project 2<span class="badge">Coming Soon</span></a>
+            <a class="list-group-item <?php echo $whichPage=='project1' ? ' active' : ''?>" href="http://p1.kushsdwa15.xyz/">Project 1</a>
+            <a class="list-group-item <?php echo $whichPage=='project2' ? ' active' : ''?>" href="http://p2.kushsdwa15.xyz/">Project 2</a>
             <a class="list-group-item disabledLink" href="http://p3.kushsdwa15.xyz/">Project 3<span class="badge">Coming Soon</span></a>
             <a class="list-group-item disabledLink" href="http://p4.kushsdwa15.xyz/">Project 4<span class="badge">Coming Soon</span></a>
           </div>
         </div>
       </div>
-      <div class="tab-pane fade" id="profile">
+      <div class="tab-pane fade <?php echo $whichPage=='project1' ? ' active in' : ''?>" id="profile">
           <h2>Profile</h2>
           <div class="row">
             <div class="col-md-6">
@@ -68,6 +82,9 @@
               <p>Ultimate Frisbee, Skiing, Traveling, MTG, Gaming, E-Sports</p>
             </div>
           </div>
+      </div>
+      <div div class="tab-pane fade <?php echo $whichPage=='project1' ? ' active in' : ''?>" id="project2">
+
       </div>
     </div>
   </div>
